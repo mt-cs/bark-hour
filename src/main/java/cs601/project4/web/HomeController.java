cd1af3c0e022772b01fc7bf1b9349e2832332015
mt-cs <Mtz31891usfca19>
2021-12-02 20:45:43 -0800

    added httpservletrequest

--- src/main/java/cs601/project4/web/HomeController.java
+++ src/main/java/cs601/project4/web/HomeController.java
@@ -1,6 +1,7 @@
 package cs601.project4.web;
 import cs601.project4.constant.LoginConstant;
 //import jakarta.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,18 +9,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
   @GetMapping("/home")
-  public String home(Model model) {
-//  public String home(Model model, HttpServletRequest request) {
-//    // retrieve the ID of this session
-//    String sessionId = request.getSession(true).getId();
-//    Object clientInfoObj =
-//        request.getSession().getAttribute(LoginConstant.CLIENT_INFO_KEY);
-//    if(clientInfoObj != null) {
-//      // already authed, no need to log in
-//      System.out.println("Client with session ID %s already exists.\n");
-//      return "redirect:/";
-//    }
-    model.addAttribute("name", "Nemo");
+//  public String home(Model model) {
+  public String home(Model model, HttpServletRequest request) {
+    // retrieve the ID of this session
+    String sessionId = request.getSession(true).getId();
+    Object clientInfoObj =
+        request.getSession().getAttribute(LoginConstant.CLIENT_INFO_KEY);
+    if(clientInfoObj != null) {
+      // already authed, no need to log in
+      System.out.println("Client with session ID %s already exists.\n");
+      return "redirect:/";
+    }
+    model.addAttribute("name", "Marisa");
     return "home";
   }
 }
