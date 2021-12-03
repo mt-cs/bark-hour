5fd3dc61aad86302b349347b7722ad46e9f44a79
mt-cs <Mtz31891usfca19>
2021-12-02 16:23:00 -0800

    fixed Spring Boot dependency, jetty server now is running

--- src/main/java/cs601/project4/web/HomeController.java
+++ src/main/java/cs601/project4/web/HomeController.java
@@ -1,22 +1,25 @@
 package cs601.project4.web;
 import cs601.project4.constant.LoginConstant;
-import cs601.project4.login.LoginServerConstants;
-import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-  public String home(Model model, HttpServletRequest request) {
-    // retrieve the ID of this session
-    String sessionId = request.getSession(true).getId();
-    Object clientInfoObj =
-        request.getSession().getAttribute(LoginConstant.CLIENT_INFO_KEY);
-    if(clientInfoObj != null) {
-      // already authed, no need to log in
-      System.out.println("Client with session ID %s already exists.\n");
-    }
-    return "redirect:/home";
+  @GetMapping("/home")
+  public String home(Model model) {
+//  public String home(Model model, HttpServletRequest request) {
+//    // retrieve the ID of this session
+//    String sessionId = request.getSession(true).getId();
+//    Object clientInfoObj =
+//        request.getSession().getAttribute(LoginConstant.CLIENT_INFO_KEY);
+//    if(clientInfoObj != null) {
+//      // already authed, no need to log in
+//      System.out.println("Client with session ID %s already exists.\n");
+//      return "redirect:/";
+//    }
+    model.addAttribute("name", "Nemo");
+    return "home";
   }
-
 }
---
+++ src/test/java/cs601/project4/web/ServerTest.java
@@ -0,0 +1,13 @@
+package cs601.project4.web;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerTest {
+
+  @Test
+  void contextLoads() {
+
+  }
+
+}
