8c4713e5008b383ef6828bc741b692f6a336eb20
mt-cs <Mtz31891usfca19>
2021-12-02 22:36:10 -0800

    added index.html and landing controller

--- src/main/java/cs601/project4/web/LandingController.java
+++ src/main/java/cs601/project4/web/LandingController.java
@@ -5,21 +5,32 @@ import cs601.project4.login.LoginServerConstants;
 import cs601.project4.login.utilities.Config;
 import cs601.project4.login.utilities.LoginUtilities;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class LandingController {
+
   @GetMapping("/")
   public String index(Model model, HttpServletRequest request) {
     // retrieve the ID of this session
     String sessionId = request.getSession(true).getId();
     Object clientInfoObj =
         request.getSession().getAttribute(LoginConstant.CLIENT_INFO_KEY);
-    if(clientInfoObj != null) {
-      // already authed, no need to log in
-      System.out.println("Client with session ID %s already exists.\n");
-      return "redirect:/home";
-    }
+//    if(clientInfoObj != null) {
+//      // already authed, no need to log in
+//      System.out.println("Client with session ID %s already exists.\n");
+//      return "redirect:/home";
+//    }
+    /** From the OpenID spec:
+     * state
+     * RECOMMENDED. Opaque value used to maintain state between the request and the callback.
+     * Typically, Cross-Site Request Forgery (CSRF, XSRF) mitigation is done by cryptographically
+     * binding the value of this parameter with a browser cookie.
+     *
+     * Use the session ID for this purpose.
+     */
     String state = sessionId;
 
     // retrieve the config info from the context
@@ -35,11 +46,13 @@ public class LandingController {
     String nonce = LoginUtilities.generateNonce(state);
 
     // Generate url for request to Slack
-    String url = LoginUtilities.generateSlackAuthorizeURL(config.getClient_id(),
+    String url = LoginUtilities.generateSlackAuthorizeURL(
+//        config.getClient_id(),
+        "2464212157.2674770528882",
         state,
         nonce,
-        config.getRedirect_url());
-
+        "https://0f6a-2601-646-202-27d0-8d2f-58a1-98c8-987f.ngrok.io/login");
+//        config.getRedirect_url());
     System.out.println(url);
 
     model.addAttribute("url", url);
--- src/main/java/cs601/project4/web/Server.java
+++ src/main/java/cs601/project4/web/Server.java
@@ -1,11 +1,29 @@
 package cs601.project4.web;
 
+import com.google.gson.Gson;
+import cs601.project4.login.LoginServerConstants;
+import cs601.project4.login.utilities.Config;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Server {
+  private static final String configFilename = "configSlack.json";
+
   public static void main(String[] args) {
+    Gson gson = new Gson();
+    Config config = null;
+    try {
+      config = gson.fromJson(new FileReader(configFilename), Config.class);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+//    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+//    context.setAttribute(LoginServerConstants.CONFIG_KEY, config);
+
     SpringApplication.run(Server.class, args);
   }
 }
